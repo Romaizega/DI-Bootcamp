0af3c051899df37f7b7dd916a6e52359a785b542
Roman Izegov <izegovrg@yandex.ru>
2025-05-11 13:33:32 +0300

    exercise 9 done

---
+++ python/week_1/d1/ExercicesXP/exercise_9.py
@@ -0,0 +1,5 @@
+your_height = int(input("Plese, write your height in cm: "))
+if your_height >= 145:
+    print("You are tall enough to ride")
+else:
+    print("You need to grow some more to ride")
