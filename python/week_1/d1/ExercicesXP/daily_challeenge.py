f8998e6d54b15e4f0217b475e926171302381b86
Roman Izegov <izegovrg@yandex.ru>
2025-05-11 14:41:03 +0300

    daily challenge done

--- python/week_1/d1/ExercicesXP/daily_challeenge.py
+++ python/week_1/d1/ExercicesXP/daily_challeenge.py
@@ -1,1 +1,15 @@
-your_word = str()
+import random
+
+your_word = input("Write your string (only 10 chars): ")
+if len(your_word) < 10:
+    print("String not long enough.")
+elif len(your_word) > 10:
+    print("String too long.")
+else:
+    print("Perfect string")
+    print(f"First character is {your_word[0]} | Last character is {your_word[-1]}")
+    for character in range(1, len(your_word) + 1):
+        print(your_word[:character])
+    new_your_word = list(your_word)
+    random.shuffle(new_your_word)
+    print(f"Your new word is {''.join(new_your_word)}")
