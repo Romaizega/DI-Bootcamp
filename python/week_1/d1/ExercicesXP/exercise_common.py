cd51b97bdeb4ca0a7214e1389a7d90ce5acbd464
Roman Izegov <izegovrg@yandex.ru>
2025-05-11 13:39:28 +0300

    created whole exercise

---
+++ python/week_1/d1/ExercicesXP/exercise_common.py
@@ -0,0 +1,46 @@
+# exercise 1
+print("Hello world "*4)
+
+# exercise 2
+print((99**3)*8)
+
+# exercise 4
+computer_brand = "IIISI"
+print(f"I have a {computer_brand} computer.")
+
+# exercise 5
+name = "Roman"
+age= 36
+shoe_size = 41
+info = f"My name is {name} and I {36} yo and I have {shoe_size} size of foot"
+print(info)
+
+# exercise 6
+a = 77
+b = 66
+if a > b:
+    print("Hello world")
+else:
+    print("Ooops")
+
+# exercise 7
+your_number = int(input("Enter your number: "))
+if your_number % 2 == 0:
+    print("Your number is even")
+else:
+    print("Your number is odd")
+
+# exercise 8
+funny_name = "Roman"
+your_name = str(input("Write your name here: "))
+if your_name == funny_name:
+    print("You're boss now")
+else:
+    print("Wrong")
+
+# exercise 9
+your_height = int(input("Plese, write your height in cm: "))
+if your_height >= 145:
+    print("You are tall enough to ride")
+else:
+    print("You need to grow some more to ride")
