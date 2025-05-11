553dbba16d96921e91702d459807f2b2e7ad36e1
Roman Izegov <izegovrg@yandex.ru>
2025-05-11 13:52:22 +0300

    some changes

--- python/week_1/d1/ExercicesXP/exercise_8.py
+++ python/week_1/d1/ExercicesXP/exercise_8.py
@@ -1,5 +1,5 @@
 funny_name = "Roman"
-your_name = str(input("Write your name here: "))
+your_name = input("Write your name here: ")
 if your_name == funny_name:
     print("You're boss now")
 else:
--- python/week_1/d1/ExercicesXP/exercise_common.py
+++ python/week_1/d1/ExercicesXP/exercise_common.py
@@ -32,7 +32,7 @@ else:
 
 # exercise 8
 funny_name = "Roman"
-your_name = str(input("Write your name here: "))
+your_name = input("Write your name here: ")
 if your_name == funny_name:
     print("You're boss now")
 else:
