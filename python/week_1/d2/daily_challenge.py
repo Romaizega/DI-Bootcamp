548e13245fa52a830b79352eba046be656d36ccb
Roman Izegov <izegovrg@yandex.ru>
2025-05-12 18:21:40 +0300

    daily challenge2 done

--- python/week_1/d2/daily_challenge.py
+++ python/week_1/d2/daily_challenge.py
@@ -1,3 +1,5 @@
+#Exercise_1
+
 number_user = int(input("Write here a number: "))
 length_user = int(input("Write here a length: "))
 my_list = []
@@ -5,3 +7,14 @@ for num in range(1,  length_user + 1):
     total = number_user * num
     my_list.append(total)
 print(my_list)
+
+#Exercise_2
+
+user_word = input("Write your word with duplicates letters: ")
+new_word = []
+prev_letter = ''
+for letter in user_word:
+    if letter != prev_letter:
+        new_word.append(letter)
+    prev_letter = letter
+print(''.join(new_word))
