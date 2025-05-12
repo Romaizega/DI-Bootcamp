108c70f48cbceca5eecf5ce79ea10fe970e16a4e
Roman Izegov <izegovrg@yandex.ru>
2025-05-12 14:19:57 +0300

    exercise madatory

---
+++ python/week_1/d2/exercise.py
@@ -0,0 +1,75 @@
+# # Exercise_1
+# my_fav_numbers = {2, 3, 5, 7 , 9}
+# my_fav_numbers.add(11)
+# my_fav_numbers.add(13)
+# my_fav_numbers.remove(13)
+# friend_fav_numbers = {6, 8, 10, 12}
+# our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
+# print(our_fav_numbers)
+
+# # Exercise_2
+# my_tuple = (2, 4, 6, 8, 9)
+# # my_tuple.add() --> Error
+
+# # Exercise_3
+# basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+# basket.remove("Banana")
+# basket.remove("Blueberries")
+# basket.append("Kiwi")
+# basket.insert(0, "Apples")
+# print(basket.count("Apples"))
+# basket.clear()
+# print(basket)
+
+# # Exercise_4
+# my_list = []
+# num = 1.5
+# while num <= 5:
+#     my_list.append(num)
+#     num += 0.5
+# print(my_list)
+
+# # Exercise_5
+#     #part_1
+# for i in range(1, 21):
+#     print(i)
+#     #part_2
+# number = list(range(1, 21))
+# for i in range(0, len(number), 2):
+#     print(number[i])
+
+# # Exercise_6
+# active = True
+# while active:
+#     your_name = input("PLease,  write here my name (enter Roman): ")
+#     if your_name != "Roman":
+#         print("Try again")
+#     else:
+#         print(f"Goobye, {your_name}")
+#         active = False
+
+# # Exercise_7
+# favorite_list = []
+# fruit = input("Write your favorite fruits, you can write through space :").lower()
+# favorite_list = fruit.split()
+# name_fruit = input("Write name of the fruit: ").lower()
+# if name_fruit in favorite_list:
+#     print("You chose one of your favorite fruits! Enjoy!")
+# else:
+
+# Exercise_8
+toppings_list = []
+active = True
+while active:
+    toppings = input("Plese, write your topping for your pizza (enter 'quit' when you are finished): ").lower()
+    if toppings == "quit":
+        active = False
+    else:
+        toppings_list.append(toppings)
+        print(f"Your {toppings} added to your pizza")
+        print(f"Your toppings: {', '.join(toppings_list)}")
+if len(toppings_list) == 0:
+        print("You did not add toppings and your pizza cost 10$")
+else:
+    price = len(toppings_list) * 2.5 + 10
+    print(f"Your pizza cost {price} $")
