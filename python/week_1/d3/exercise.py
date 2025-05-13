2920885ae43c05bcbf3a17b143fc3e19e30a4164
Roman Izegov <izegovrg@yandex.ru>
2025-05-13 15:24:53 +0300

    exercise2 done

--- python/week_1/d3/exercise.py
+++ python/week_1/d3/exercise.py
@@ -4,3 +4,22 @@ values = [10, 20, 30]
 
 new_dic = {key: value for key, value in zip(keys, values)}
 print(new_dic)
+
+# Exercise_2
+total_cost = 0
+family = {"rick": 43,
+          "beth": 13, 
+          "morty": 5, 
+          "summer": 8}
+
+for name, age  in family.items():
+    if age <=3:
+        total_cost =+ 0
+        print("Ticket costs is free")
+    elif age >=12:
+        total_cost += 15
+        print(f"Ticket for {name.capitalize()} costs $15")
+    else:
+        total_cost += 10
+        print(f"Ticket for {name.capitalize()} costs $10")
+print(f"Total cost is ${total_cost}")
