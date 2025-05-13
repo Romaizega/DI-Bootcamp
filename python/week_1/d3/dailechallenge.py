18fa53782e6767901f31dfe981d2d9adcbd90d1e
Roman Izegov <izegovrg@yandex.ru>
2025-05-13 21:25:41 +0300

    deleted comments

--- python/week_1/d3/dailechallenge.py
+++ python/week_1/d3/dailechallenge.py
@@ -1,14 +1,14 @@
-# # Exercise_1
-# user_word = ''
-# while user_word != "quit":
-#     user_word = input("Write your word here (or type 'quit'): ")
-#     my_dict = {}
-#     for index, char in enumerate(user_word):
-#         if char in my_dict:
-#             my_dict[str(char)].append(int(index))
-#         else:
-#             my_dict[str(char)] = [int(index)]
-#     print(my_dict)
+# Exercise_1
+user_word = ''
+while user_word != "quit":
+    user_word = input("Write your word here (or type 'quit'): ")
+    my_dict = {}
+    for index, char in enumerate(user_word):
+        if char in my_dict:
+            my_dict[str(char)].append(int(index))
+        else:
+            my_dict[str(char)] = [int(index)]
+    print(my_dict)
 
 # Exercise_2
 
