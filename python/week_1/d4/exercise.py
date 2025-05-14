3d9c1a39bef1ef194b561f85531148e41ef828c4
Romaizega <izegovrg@yandex.ru>
2025-05-14 17:06:54 +0300

    exercise 4 done

--- python/week_1/d4/exercise.py
+++ python/week_1/d4/exercise.py
@@ -1,4 +1,27 @@
+import random
+
 # Exercise_1
 def display_message():
     print("I am learning about functions in Python.")
 display_message()
+
+# Exercise_2
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title}")
+favorite_book("Alice in Wonderland")
+
+# Exercise_3
+def describe_city(city, country = "Unknown"):
+    print(f"{city} is in {country}")
+describe_city("Reykjavik", "Iceland")
+describe_city("Paris")
+
+# Exercise_4
+def random_namber(your_number):
+    number = random.randint(1, 100)
+    if your_number == number:
+        print("Succes!")
+    else:
+        print(f"Fail! your number: {your_number}, but Random number: {number}")
+random_namber(4)
