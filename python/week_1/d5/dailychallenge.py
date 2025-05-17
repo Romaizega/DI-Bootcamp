c38332c9ee0be187783fd294ffd0050663eb674b
Romaizega <izegovrg@yandex.ru>
2025-05-17 10:37:18 +0300

    exercise2 of DCH done

--- python/week_1/d5/dailychallenge.py
+++ python/week_1/d5/dailychallenge.py
@@ -1,4 +1,14 @@
 # Exercise_1
-user_words = input(("Write your words: ").lower()).split()
+user_words = input("Write your words: ").split(",")
 user_words.sort()
 print(",".join(user_words))
+
+# Exercise_2
+def longest_word(sentence):
+    new_sentence = sentence.split()
+    long_word = ""
+    for words in new_sentence:
+            if len(words) > len(long_word):
+                  long_word = words
+    print(f" the longest word is {long_word}")
+longest_word("Forgetfulness is by all means powerless!")
