45e719bc449d1fcf6e5fc5ffdad0dc4c4f4d32ce
Romaizega <izegovrg@yandex.ru>
2025-05-19 17:16:01 +0300

    exercise 2 XP done

---
+++ python/week_2/d2/XP/exercise_2.py
@@ -0,0 +1,29 @@
+class Dog:
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+
+    def bark(self):
+        return f"{self.name} is barking"
+    
+    def run_speed(self):
+        return self.weight / self.age * 10
+    
+    def fight(self, other_dog):
+        self.other_dog = other_dog
+        power_dog = self.run_speed() * self.weight
+        power_other_dog = other_dog.run_speed() * other_dog.weight
+        if power_dog > power_other_dog:
+            return f"{self.name} won!"
+        elif power_dog < power_other_dog:
+            return f"{other_dog.name} won!"
+
+    
+dog1 = Dog("Rex", 4, 54)
+dog2 = Dog("Pex", 3, 65)
+dog3 = Dog("Pig", 7, 32)
+
+print(dog1.bark())
+print(dog2.run_speed())
+print(dog1.fight(dog2))
