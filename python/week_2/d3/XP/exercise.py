877472726cb8a5385cb7e57d8f5bf0abdbca64b9
Romaizega <izegovrg@yandex.ru>
2025-05-20 17:35:29 +0300

    exercise7 done

--- python/week_2/d3/XP/exercise.py
+++ python/week_2/d3/XP/exercise.py
@@ -1,4 +1,9 @@
+from faker import Faker
 import random, string
+from datetime import datetime
+
+from tabulate import tabulate
+
 
 # Exercise 1
 class Currency:
@@ -48,3 +53,48 @@ def random_string():
         my_string += random.choice(my_letters)
     print(my_string)
 random_string()
+
+
+# Exercise # 4
+
+def current_date():
+    time_now = datetime.now()
+    my_time = time_now.strftime("%d.%m.%Y, %H:%M")
+    print(f"It's time: {my_time}")
+current_date()
+
+# Exercise 5
+
+def expect_date():
+    exp_date = datetime(2026, 1, 1)
+    time_now = datetime.now()
+    diff_time = exp_date - time_now
+    print(diff_time)
+expect_date()
+
+
+# Exercise 6
+
+def how_many_time(birthday):
+    time_now = datetime.now()
+    whole_date = time_now - birthday
+    whole_minute = whole_date.total_seconds() // 60
+    print(whole_date)
+    print(whole_minute)
+how_many_time(birthday = datetime(1989, 3, 23))
+
+# Exercise 7
+
+fake = Faker()
+data = []
+def create_faker_users():
+    for _ in range(5):
+        name = fake.name()
+        adress = fake.address()
+        language_code = fake.language_code()
+        data.append([name, adress, language_code])
+    print(data)
+    headers = ["Name", "Address", "Language"]
+    data_dict = [dict(zip(headers, i)) for i in data]
+    print(data_dict)
+create_faker_users()
