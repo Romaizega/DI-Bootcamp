47da549ac82212ffbbfaaf8615e2ebc224ab51e2
Romaizega <izegovrg@yandex.ru>
2025-05-20 14:59:50 +0300

    exercise 1 done

---
+++ python/week_2/d3/exercise.py
@@ -0,0 +1,36 @@
+class Currency:
+    def __init__(self, currency, amount):
+        self.currency = currency
+        self.amount = amount
+        
+    def __str__(self):
+        return f'{self.amount} {self.currency}'
+    
+    def __repr__(self):
+        return self.__str__()
+
+    def __int__(self):
+        return self.amount
+
+    def __add__(self, other):
+        if isinstance(other, Currency):
+            if self.currency != other.currency:
+                raise TypeError("Cannot add different currencies")
+            return self.amount + other.amount
+        elif isinstance(other, int):
+            return self.amount + other
+
+    def __iadd__(self, other):
+        if isinstance(other, Currency):
+            if self.currency != other.currency:
+                raise TypeError("Cannot add different currencies")
+            self.amount += other.amount
+        elif isinstance(other, int):
+            self.amount += other
+        return self
+    
+
+c1 = Currency('dollar', 5)
+c2 = Currency('dollar', 10)
+c3 = Currency('shekel', 1)
+c4 = Currency('shekel', 10)
