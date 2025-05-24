e46dc77b9a9964d619afdd90ae31017e34b65ff6
Romaizega <izegovrg@yandex.ru>
2025-05-24 15:54:31 +0300

    created class Deck

--- python/week_2/d5/dailychallenge.py
+++ python/week_2/d5/dailychallenge.py
@@ -1,12 +1,33 @@
+import random
+
 class Card:
     def create_deck(self):
         suit  =  ["Hearts", "Diamonds", "Clubs", "Spades"]
         value = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
         self.deck = [(sut, val) for sut in suit for val in value]
+        return self.deck
 
-    def show_deck(self):
-        print(self.deck)
+    def __str__(self):
+        return self.deck
 
-card = Card()
-card.create_deck()
-card.show_deck()
+class Deck:
+    def shuffle (self):
+        card = Card()
+        self.deck = card.create_deck()
+        if len(self.deck) == 52:
+            self.new_deck = random.shuffle(self.deck)
+            return self.deck
+
+        
+    def deal(self):
+        # while len(self.deck) != 0:
+            choice_card = random.choice(self.deck)
+            for cards in self.deck:
+                if cards == choice_card:
+                    self.deck.remove(choice_card)
+            print(choice_card)
+            print(len(self.deck))
+
+decker = Deck()
+decker.shuffle()
+decker.deal()
