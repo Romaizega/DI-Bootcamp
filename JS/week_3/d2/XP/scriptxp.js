95d1a79e1f72c5d4ab7094ed1014753f20901c9f
Romaizega <izegovrg@yandex.ru>
2025-05-26 13:25:14 +0300

    exercise 1 done

---
+++ JS/week_3/d2/XP/scriptxp.js
@@ -0,0 +1,15 @@
+// Exercise 1
+
+function displayNumbersDivisible() {
+    let sum = 0
+    for (let i = 0; i <= 500; i++)
+        if (i % 23 == 0) {
+            console.log(i); 
+            sum += i           
+        }
+        console.log(sum);
+        
+    }
+displayNumbersDivisible()
+
+// Exercise 2
