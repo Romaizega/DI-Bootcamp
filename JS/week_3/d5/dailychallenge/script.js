ea7228a2fe1a7bc7357d2cef4ad26c174b95ad2d
Romaizega <izegovrg@yandex.ru>
2025-06-01 09:12:46 +0300

    created second function

--- JS/week_3/d5/dailychallenge/script.js
+++ JS/week_3/d5/dailychallenge/script.js
@@ -1,5 +1,7 @@
 const tasks = [];
 
+let taskId = 0;
+
 const checInput = document.getElementsByTagName("input")
 const butt = document.getElementsByTagName("button")
 const myDiv = document.getElementsByClassName("listTasks")
@@ -9,12 +11,19 @@ butt[0].addEventListener("click", function(e){
     const userInput = checInput[0].value;
     if (userInput.trim().length === 0)
     {alert("Please fill the field")}
-    else {tasks.push(userInput)};
+    else {tasks.push({
+        task_id: taskId,
+        text: userInput,
+        done: false,}
+    )};
     const myNewDiv = document.createElement("div");
+    myNewDiv.setAttribute("data-task-id", taskId);
+    taskId++;
     const myBox = document.createElement("input");
     const p = document.createElement("p");
     const myIelem = document.createElement("i");
     myBox.setAttribute("type", "checkbox");
+    myBox.addEventListener("change", doneTask)
     myIelem.className = "fa fa-times";
     myIelem.style.cursor = "pointer";
     myIelem.addEventListener("click", () => {
@@ -27,4 +36,21 @@ butt[0].addEventListener("click", function(e){
     p.textContent = userInput;
     checInput[0].value = "";
     myNewDiv.className = "task-item";
+    console.log(tasks);
+    
 })
+
+function doneTask(event) {
+    const taskDiv = event.target.parentElement;
+    const id = parseInt(taskDiv.getAttribute("data-task-id"))
+    const task = tasks.find(t => t.task_id ===id);
+    task.done = event.target.checked;
+    const p = taskDiv.querySelector("p");
+    if (task.done) {
+    p.style.textDecoration = "line-through";
+    p.style.color = "red";
+    } else {
+    p.style.textDecoration = "none";
+    p.style.color = "black";
+}    
+}
