be77df255b1e7e476574dbf833bb53b01b8f5369
Romaizega <izegovrg@yandex.ru>
2025-07-08 11:23:03 +0300

    fixed

--- database/week7/d2/daily_challenge/config/server.js
+++ database/week7/d2/daily_challenge/config/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const user_routers = require("../routers/userRouters")
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json())
 app.use('/users', user_routers)
 app.listen(PORT, ()=>{
