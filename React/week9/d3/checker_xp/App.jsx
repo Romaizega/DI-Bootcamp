0b379cef40ba70695c09609b67d57a35468a529c
Romaizega <izegovrg@yandex.ru>
2025-07-28 13:07:15 +0300

    xp done

---
+++ React/week9/d3/checker_xp/App.jsx
@@ -0,0 +1,17 @@
+import Counter from './feature/counter/Counter'
+import Users from './feature/user/Users'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Users/>
+
+    </>
+  )
+}
+
+export default App
